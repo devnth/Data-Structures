e362525b8abc19eb30a53cec5a39293094e250f9
Dev <devanath@Devs-MacBook-Air.local>
2023-01-23 22:33:59 +0530

    highest mountain peak

---
+++ Binary Search/highestMountainPeak.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	data := []int{2, 4, 5, 6, 7, 4, 3}
+
+	fmt.Printf("Index of Target %d", ans(data))
+
+}
+
+func ans(data []int) int {
+
+	start := 0
+	end := len(data) - 1
+
+	for start < end {
+
+		mid := start + (end-start)/2
+
+		if data[mid] < data[mid+1] {
+			start = mid + 1
+		} else {
+			end = mid
+		}
+
+	}
+	return data[start]
+}
