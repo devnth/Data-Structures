751c29d376b8aee45d2a4dfed2223373aa5bf443
Dev <devanath@Devs-MacBook-Air.local>
2023-01-23 16:39:18 +0530

    binary search optimised

--- Binary Search/Next Character/nextSmallestCharacter.go
+++ Binary Search/Next Character/nextSmallestCharacter.go
@@ -16,29 +16,25 @@ func main() {
 
 }
 
-func nextSmallestCharacter(characters []byte, target byte) string {
+func nextGreatestCharacter(characters []byte, target byte) string {
 
 	start := 0
 
-	end := len(characters) - 1
+	length := len(characters)
+
+	end := length - 1
 
 	for start <= end {
 
 		middle := start + (end-start)/2
 
-		if characters[middle] == target {
-
-			if middle == len(characters)-1 {
-				return string(characters[0])
-			}
-			return string(characters[middle+1])
-		} else if target <= characters[middle] {
+		if target < characters[middle] {
 			end = middle - 1
 		} else {
 			start = middle + 1
 		}
 	}
 
-	return string(characters[start])
+	return string(characters[start%length])
 
 }
